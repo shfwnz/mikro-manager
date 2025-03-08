eeaa14f2c893aff294759ccd998df1292e068041
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-09 06:31:04 +0700

    refactor: oraganizing function

--- views/ip_address.py
+++ views/ip_address.py
@@ -50,18 +50,17 @@ def get_ip(output):
         parts = line.split()
         if len(parts) >= 4:
             index = parts[0].split('=')[-1]  
-            
             address = parts[1].split('=')[-1]
             network = parts[2].split('=')[-1]
             interface = parts[3].split('=')[-1]
-            status_code = ""
+            
+            status_code = parts[0][0]
             
             status_text = {
-                "": "Active",
                 "D": "Dynamic",
                 "X": "Disabled",
                 "I": "Invalid"
-            }.get(status_code, "Unknown")
+            }.get(status_code, "Active")
             
             cols = st.columns([3,2,1.5,1,1])
             with cols[0]: st.write(address)
@@ -80,15 +79,7 @@ def show_ip(client):
     output, _ = execute_command(client, "/ip address print terse")
     get_ip(output)
     
-def ip_conf(client):
-    interfaces = get_interface(client)
-    
-    if not interfaces:
-        st.warning("Interfaces not found")
-        return
-            
-    selected_interface = st.selectbox("Select Interface:", interfaces, placeholder="choose an interfaces", index=None)
-    
+def enable_disable_interface_btn(client, selected_interface):
     message_success = ""
     message_error = ""
     
@@ -96,9 +87,8 @@ def ip_conf(client):
     with col1:
         if st.button("Enable Interface", use_container_width=True):
             enable_command = f"/interface enable {selected_interface}"
-            stdin, stdout, stderr = client.exec_command(enable_command)
-            output = stdout.read().decode().strip()
-            error = stderr.read().decode().strip()
+            _ , error = execute_command(client, enable_command)
+            
             if error:
                 message_error = f"Unable to enable {selected_interface}: {error}"
             else:
@@ -106,9 +96,8 @@ def ip_conf(client):
     with col2:
         if st.button("Disable Interface", use_container_width=True):
             disable_command = f"/interface disable {selected_interface}"
-            stdin, stdout, stderr = client.exec_command(disable_command)
-            output = stdout.read().decode().strip()
-            error = stderr.read().decode().strip()
+            _ , error = execute_command(client, disable_command)
+            
             if error:
                 message_error = f"Unable to disable {selected_interface}: {error}"
             else:
@@ -119,41 +108,52 @@ def ip_conf(client):
     elif message_success:
         st.success(message_success) 
     
+def apply_conf(client, selected_interface, ip_address, subnet_mask, remove_old):
+    try:
+        cidr = subnet_mask_to_cidr(subnet_mask)
+        ip_with_subnet = f"{ip_address}/{cidr}"
+        
+        if remove_old:
+            remove_command = f"/ip address remove [find interface={selected_interface}]"
+            execute_command(client, remove_command)
+            st.warning(f"Removing old IPs on {selected_interface}...")
+        
+        command = f"/ip address add address={ip_with_subnet} interface={selected_interface}"
+        _ , error = execute_command(client, command)
+        
+        if error:
+            st.error(f"Error: {error}")
+        else:
+            st.success(f"New IP {ip_with_subnet} applied to {selected_interface}")
+            rerun_after(3)
+        
+    except Exception as e:
+        st.error(f"Failed to Set IP: {e}")
+    
+def ip_conf(client):
+    interfaces = get_interface(client)
+    
+    if not interfaces:
+        st.warning("Interfaces not found")
+        return
+            
+    selected_interface = st.selectbox("Select Interface:", interfaces, placeholder="choose an interfaces", index=None)
+    
+    enable_disable_interface_btn(client, selected_interface)
+    
     ip_address = st.text_input("IP Address:", placeholder="Enter the IP address of the device", help="Example: 192.168.88.1")
     subnet_mask = st.text_input("Subnet:", placeholder="Enter a subnet mask for the device", help="Example: 255.255.255.0")
     remove_old = st.checkbox("Remove old IP before applying", False)
     
-    st.subheader("Apply Configuration")
     if st.button("Apply Configuration"):
-        try:
-            cidr = subnet_mask_to_cidr(subnet_mask)
-            ip_with_subnet = f"{ip_address}/{cidr}"
-            
-            if remove_old:
-                remove_command = f"/ip address remove [find interface={selected_interface}]"
-                client.exec_command(remove_command)
-                st.warning(f"Removing old IPs on {selected_interface}...")
-            
-            command = f"/ip address add address={ip_with_subnet} interface={selected_interface}"
-            stdin, stdout, stderr = client.exec_command(command)
-            stdout.channel.recv_exit_status()
-            output = stdout.read().decode().strip()
-            error = stderr.read().decode().strip()
-            
-            if error:
-                st.error(f"Error: {error}")
-            else:
-                st.success(f"New IP {ip_with_subnet} applied to {selected_interface}")
-                st.rerun()
-            
-        except Exception as e:
-            st.error(f"Failed to Set IP: {e}")
-
+        apply_conf(client, selected_interface, ip_address, subnet_mask, remove_old)
+    
 if 'ssh_connection' not in st.session_state or not st.session_state['ssh_connection']:
     st.warning("Please connect to the Router first")
 else:
     st.header("IP Address Configuration")
     tab1, tab2 = st.tabs(["Show IP", "Add IP"])
+    
     try:
         client = st.session_state.get('ssh_client', None)
         if client is None:
