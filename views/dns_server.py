267b2f7c4304229ff44e6bb75eae22a0f9376a56
OyShan1 <ilhamdzaky2007@gmail.com>
2025-03-09 13:21:51 +0700

    fix: dns server conf

--- views/dns_server.py
+++ views/dns_server.py
@@ -1,23 +1,27 @@
 import streamlit as st
 
+def execute_command(client, command):
+    stdin, stdout, stderr = client.exec_command(command)
+    return stdout.channel.recv_exit_status(), stdout.read().decode().strip(), stderr.read().decode().strip()
+
 if 'ssh_connection' not in st.session_state or not st.session_state['ssh_connection']:
     st.warning("Please connect to the Router first")
 else:
     st.header("Setting DNS")
+    tab1, tab2 = st.tabs(["Server Provider", "Custome Name Server"])
 
     dns_options = {
         "Google DNS": "8.8.8.8,8.8.4.4",
         "Cloudflare": "1.1.1.1,1.0.0.1",
         "OpenDNS": "208.67.222.222,208.67.220.220",
-        "Other": ""
     }
-
-    selected_server = st.selectbox("Server Provider:", list(dns_options.keys()))
     
-    if selected_server == "Other":
-        custom_dns = st.text_input("Enter custom name servers (comma-separated)", placeholder="1.1.1.1, 8.8.8.8")
-    else:
-        custom_dns = dns_options[selected_server]
+    with tab1:
+        select_server = st.selectbox("Server Provider:", list(dns_options.keys()))
+        selected_server = dns_options[select_server]
+        
+    with tab2:
+        custom_dns_input = st.text_input("Enter custom name servers (comma-separated)", placeholder="1.1.1.1, 8.8.8.8")
     
     allow_remote_request = st.checkbox("allow remote request", True)
     
@@ -27,18 +31,14 @@ else:
             if client is None or client.get_transport() is None or not client.get_transport().is_active():
                 st.error("Unable to connect to the router. Please reconnect.")
             else:
-                command = f"/ip dns set servers={custom_dns} allow-remote-requests={'yes' if allow_remote_request else 'no'}"
+                command = f"/ip dns set servers={custom_dns_input} allow-remote-requests={'yes' if allow_remote_request else 'no'}"
                 
-                stdin, stdout, stderr = client.exec_command(command)
-                stdout.channel.recv_exit_status()
-                
-                output = stdout.read().decode().strip()
-                error = stderr.read().decode().strip()
+                _, output, error = execute_command(client, command)
 
                 if error:
                     st.error(f"Error: {error}")
                 else:
-                    st.success(f"DNS applied: {custom_dns}")
+                    st.success(f"DNS applied: {custom_dns_input}")
                     if output:
                         st.text(f"Response: {output}")
 
